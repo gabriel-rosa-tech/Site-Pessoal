2ca560c34afa98bf2cb1e26b1f30b3e71827b59b
gabrosa <gabriel.melo@bandtec.com.br>
2023-01-05 19:08:00 -0300

    Api - Insert Criatura

---
+++ site-pessoal/Controllers/CriaturaController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using site_pessoal.Models;
+
+namespace site_pessoal.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CriaturaController : ControllerBase
+    {
+
+        List<CriaturaModel> listaCriaturas = new List<CriaturaModel>();
+
+        [HttpGet("{id:int}")]
+        public CriaturaModel[] Get(int id)
+        {
+            return listaCriaturas.Where(c => c.Id == id).ToArray();
+        }
+
+        [HttpPost]
+        public bool PostCriatura(CriaturaModel criatura)
+        {
+            if (criatura != null)
+            {
+                listaCriaturas.Add(criatura);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ site-pessoal/Models/CriaturaModel.cs
@@ -0,0 +1,11 @@
+﻿namespace site_pessoal.Models
+{
+    public class CriaturaModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public int Imagem { get; set; }
+
+    }
+}
